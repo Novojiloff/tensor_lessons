061bebada777b1ed3abc907479fbca0c0cc3708e
Novojiloff <49559577+Novojiloff@users.noreply.github.com>
2021-03-09 00:17:57 +0300

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -113,3 +113,29 @@
 
 # decode = decode(encode, step)
 # print(decode)
+
+
+import math
+
+# print('Решение квадратного уравнения вида Ax^2 + Bx + C = 0\n')
+
+# a = int(input('Введите А: '))
+# b = int(input('Введите B: '))
+# c = int(input('Введите C: '))
+
+# discriminant = b**2 - (4 * a * c)
+
+# if discriminant > 0:
+#     x1 = -b + math.sqrt(discriminant) / (2 * a)
+#     x2 = -b - math.sqrt(discriminant) / (2 * a)
+#     print('Дискриминант уравнения больше 0. Поэтому корней два.')
+#     print(f'x1 = {round(x1, 2)}\nx2 = {round(x2, 2)}')
+# elif discriminant < 0:
+#     print('Дискриминант уравнения меньше 0. Поэтому корни комплексные.')
+#     x1 = complex(-b + discriminant ** 0.5) / (2 * a)
+#     x2 = x1.conjugate()
+#     print(f"x1 = {x1:.3f}, \nx2 = {x2:.3f}")
+# else:
+#     x1 = - b / (2 * a)
+#     print('Дискриминант уравнения равен 0. Поэтому корень один.')
+#     print(f'x = {round(x1, 2)}')
